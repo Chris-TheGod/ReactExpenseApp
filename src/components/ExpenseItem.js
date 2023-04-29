6af84fcb61cdfbdd0ef9caf8f170fa171671d0c2
Kristjan Lääne <xonproject@gmail.com>
2023-04-29 16:40:25 +0300

    react expenses app changes 1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,13 @@
+import { ExpenseDate } from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-export const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expeseAmount = 294.67;
+export const ExpenseItem = ({ title, amount, date }) => {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expeseAmount}$</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}$</div>
       </div>
     </div>
   );
