f853e3a0716f80760c7d2540e6ecfbd75b14c5a2
Kristjan Lääne <xonproject@gmail.com>
2023-04-29 15:06:39 +0300

    react expenses app

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import "./ExpenseItem.css";
+
+export const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expeseAmount = 294.67;
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expeseAmount}$</div>
+      </div>
+    </div>
+  );
+};
