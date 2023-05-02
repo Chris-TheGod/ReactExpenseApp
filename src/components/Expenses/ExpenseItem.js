8352a811e5ed452ff6ff7f4f72a25b181c3bccad
Kristjan Lääne <xonproject@gmail.com>
2023-05-02 20:01:22 +0300

    learned about state and changed some text onClick

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,15 @@
+import React, { useState } from "react";
 import { Card } from "../UI/Card";
 import { ExpenseDate } from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-export const ExpenseItem = ({ title, amount, date }) => {
+export const ExpenseItem = ({ title: props_title, amount, date }) => {
+  const [title, setTitle] = useState(props_title);
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
@@ -10,6 +17,7 @@ export const ExpenseItem = ({ title, amount, date }) => {
         <h2>{title}</h2>
         <div className="expense-item__price">{amount}$</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
